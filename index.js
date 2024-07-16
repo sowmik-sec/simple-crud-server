bc9f3a25638f792c3db29aafabe324bbfddcf0ea
sowmik-sec <sowmik.sec@gmail.com>
2024-07-16 11:17:52 +0600

    get multiple users from the db

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
     const database = client.db("usersDB");
     const userCollection = database.collection("users");
 
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log("new user", user);
