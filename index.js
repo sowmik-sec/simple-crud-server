2e9d634f0656e6435bbfb7feb6a795e4f37f5bed
sowmik-sec <sowmik.sec@gmail.com>
2024-07-16 12:03:52 +0600

    load single user from db

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log("new user", user);
